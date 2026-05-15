b42eec2a420178a19f8d3dad88b1ca1c1a0af430
danelfernandez <danelfernandez@users.noreply.github.com>
2026-05-15 10:46:47 +0000

    deploy: 179e1bf02fc5ce988d331d820e6341a096ba7668

--- doxygen/dir_5eb159725f84c66aafd839904a4acdd0.js
+++ doxygen/dir_5eb159725f84c66aafd839904a4acdd0.js
@@ -1,5 +1,4 @@
 var dir_5eb159725f84c66aafd839904a4acdd0 =
 [
-    [ "bundles", "dir_e21f4ee1c22940755bba28a4171f9437.html", null ],
     [ "java", "dir_fd3f6763802dee1ad875f6c80eac0bda.html", "dir_fd3f6763802dee1ad875f6c80eac0bda" ]
 ];
--- doxygen/navtreedata.js
+++ doxygen/navtreedata.js
@@ -25,7 +25,6 @@
 var NAVTREE =
 [
   [ "CalendarioApp", "index.html", [
-    [ "README", "md_src_2main_2bundles_2_r_e_a_d_m_e.html", null ],
     [ "Packages", "namespaces.html", [
       [ "Package List", "namespaces.html", null ]
     ] ],
