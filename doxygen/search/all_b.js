872de85caadc9125e300db0bc4ada671f1e9e037
danelfernandez <danelfernandez@users.noreply.github.com>
2026-05-15 13:37:39 +0000

    deploy: fded091137051f0f07f68e69ac12fd1c5e959a80

--- doxygen/search/all_b.js
+++ doxygen/search/all_b.js
@@ -1,7 +1,4 @@
 var searchData=
 [
-  ['registroview_0',['registroview',['../classcom_1_1example_1_1restservice_1_1_u_i_1_1_registro_view.html',1,'com.example.restservice.UI.RegistroView'],['../classcom_1_1example_1_1restservice_1_1_u_i_1_1_registro_view.html#a15bbaf6a4d4daadaa46173c08032fe1c',1,'com.example.restservice.UI.RegistroView.RegistroView()']]],
-  ['registroview_2ejava_1',['RegistroView.java',['../_registro_view_8java.html',1,'']]],
-  ['restserviceapplication_2',['RestServiceApplication',['../classcom_1_1example_1_1restservice_1_1_rest_service_application.html',1,'com::example::restservice']]],
-  ['restserviceapplication_2ejava_3',['RestServiceApplication.java',['../_rest_service_application_8java.html',1,'']]]
+  ['quality_20manager_0',['PSyC Quality Manager',['../index.html',1,'']]]
 ];
