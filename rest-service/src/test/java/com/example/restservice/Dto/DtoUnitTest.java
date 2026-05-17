f759a3e6d44ebe14b146b7704b3102fea4c64b84
maria.tejedor <maria.tejedor@opendeusto.es>
2026-05-17 11:17:53 +0200

    configuradas exclusiones UI en SonarCloud

--- rest-service/src/test/java/com/example/restservice/Dto/DtoUnitTest.java
+++ rest-service/src/test/java/com/example/restservice/Dto/DtoUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.restservice;
+package com.example.restservice.Dto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
